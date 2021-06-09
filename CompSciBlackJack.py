cb832f7827e301bd850da8874a80d8baf8b2638e
GabrielGhrayeb <gabrielghrayebg@gmail.com>
2021-06-09 17:44:31 -0400

    Update CompSciBlackJack.py

--- CompSciBlackJack.py
+++ CompSciBlackJack.py
@@ -2,59 +2,105 @@ import random
 import math
 
 
-deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "Q", "K"]*4
 dealHand = []
 playing = True
 playerHand = []
 playerMoney = 1000
+dealerTotal = 0
+playerTotal = 0
+
+
 
 def default():
-    pass
+    deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4
+    dealHand.clear()
+    playerHand.clear()
+    playing = True
+    playerMoney = 1000
 
 def shuffle(alist):
     deck = random.shuffle(alist)
 
 def placeBets():
-    print("Your Total Is ", playerMoney)
-    bet = int(input("How much would you like to bet?"))
-    print("Your Total Is Now ", playerMoney-bet)
-    
+    error = False
+    while(error == True):
+        try:
+            print("Your Total Is ", playerMoney)
+            bet = int(input("How much would you like to bet?"))
+            if bet >= playerMoney:
+                print("All In")
+                bet = playerMoney
+            print("Your Total Is Now ", playerMoney-bet)
+            error = False
+        except:
+            error == True
+
 
 def dealCards():
     shuffle(deck)
+
     for card in deck[:2]:
+        # if card == 11:
+        #     card = "A"
+        
+        # if card == 12:
+        #     card = "J"
+
+        # if card == 13:
+        #     card = "Q"
+
+        # if card == 14:
+        #     card = "K"
+                
         #print(card, end=" ")
         dealHand.append(card)
         deck.pop(deck.index(card))
+
+        # for royal in 
+    #
     for card in deck[:2]:
+        deck.pop(deck.index(card))
         #print(card, end=" " )
         playerHand.append(card)
-        deck.pop(deck.index(card))
+        #deck.pop(deck.index(card))
 
     print("Dealer's Hand Cards: ", dealHand)
     print("Dealer's Hand Total:" , )
     print("Your Hand Cards: ", playerHand)
 
 def score(card):
-    if card == 11:
-        card = "A"
+    pass
+
+def totalScore():
+    for i in dealHand:
+        if i == "J" or i == "Q" or i == "K":
+            dealerTotal+= 10
+        elif i == "A":
+            if dealerTotal >= 11:
+                dealerTotal+= 1
+            else:
+                dealerTotal+= 11
+        else:
+            dealerTotal+= i
+
+    for i in playerHand:
+        if i == "J" or i == "Q" or i == "K":
+            playerTotal+= 10
+        elif i == "A":
+            if playerTotal >= 11:
+                playerTotal+= 1
+            else:
+                playerTotal+= 11
+        else:
+            playerTotal+= i
+    print("Dealer Total: ", dealerTotal)
+    print("Player Total: ", playerTotal)
         
-    if card == 12:
-        card = "J"
-
-    if card == 13:
-        card = "Q"
-
-    if card == 14:
-        card = "K"
-
-
 def playAgain():
     play=input("Would You Like to Play Again? Y/N: ").lower()
     if play == "y":
-        dealHand.clear()
-        playerHand.clear()
-        deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4
+        default()
     if play == "n":
         playing = False
         print("Thanks For Playing!")
@@ -62,12 +108,14 @@ def playAgain():
         print("Money: ", playerMoney)
         exit()
 
-    
+def scoreCheck():
+    pass
 
 def main():
     while(playing == True or playerMoney > 0):
         placeBets()
         dealCards()
+        scoreCheck()
         playAgain()
 
 if __name__ == '__main__':
